2eb07ae247250c32fb0a3ffdfa9a779de40e3f64
rmborges <rafael.m.borges@tecnico.ulisboa.pt>
2017-12-10 16:55:27 +0000

    bscXfer python

--- bsc_i2c.py
+++ bsc_i2c.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
-
 import time
 import pigpio
 
-I2C_ADDR=9
+I2C_ADDR=0x13
 
 def i2c(id, tick):
-   global pi
+    global pi
 
-   s, b, d = pi.bsc_i2c(I2C_ADDR)
+    s, b, d = pi.bsc_i2c(I2C_ADDR)
+    if b:
+        if d[0] == ord('t'): # 116 send 'HH:MM:SS*'
 
-   if b:
+            print("sent={} FR={} received={} [{}]".
+               format(s>>16, s&0xfff,b,d))
 
-     print(d[:-1])
+            s, b, d = pi.bsc_i2c(I2C_ADDR,
+               "{}*".format(time.asctime()[11:19]))
+
+        elif d[0] == ord('d'): # 100 send 'Sun Oct 30*'
+
+            print("sent={} FR={} received={} [{}]".
+               format(s>>16, s&0xfff,b,d))
+
+            s, b, d = pi.bsc_i2c(I2C_ADDR,
+               "{}*".format(time.asctime()[:10]))
 
 pi = pigpio.pi()
 
