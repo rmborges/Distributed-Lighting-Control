cd369ececddfb8aa650beff6218f92c348655291
pgmacedo <paulogranjamacedo@gmail.com>
2017-12-12 15:13:44 +0000

    juntar ficheiros

--- messages.cpp
+++ messages.cpp
@@ -1,8 +1,9 @@
 #include "messages.h"
 
-messages::messages();
+messages::messages()
+{}
 
-messages::print_message(std::string message){
+void messages::print_message(std::string message){
 
 	std::cout << message << std::endl;
 
