12920fd92c3c446cd32630bd9d4207c223f1b13b
pgmacedo <34351322+pgmacedo@users.noreply.github.com>
2017-12-12 14:49:09 +0000

    Add files via upload

---
+++ messages.cpp
@@ -0,0 +1,7 @@
+#include "messages.h"
+
+messages::print_message(std::string message){
+
+	cout << message << endl;
+
+}
---
+++ messages.h
@@ -0,0 +1,24 @@
+#ifndef MESSAGES_H_
+#define MESSAGES_H_
+
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include <iostream>
+#include <unistd.h>
+#include "serial_send.h"
+
+using namespace boost::asio;
+using ip::tcp;
+using boost::asio::deadline_timer;
+
+class messages {
+
+public:
+	void print_message(std::string message);
+
+
+};
+
+#endif
