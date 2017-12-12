b818db7c6c6f478885da20da0f8948f5439bde0d
rmborges <rafael.m.borges@tecnico.ulisboa.pt>
2017-12-12 14:55:27 +0000

    li

--- arduino.cpp
+++ arduino.cpp
@@ -6,24 +6,3 @@ occupancy(true)
 {
 
 }
-
-
-	static bool restart;
-	static double inst_power_system;
-	static double acc_comfort_error_system;
-	static double acc_comfort_var_system;
-
-	//for each arduino
-	double illuminance;
-	double duty_cycle;
-	bool occupancy;
-	double lower_bound;
-	double ext_illuminance;
-	double illuminance_ref;
-	double inst_power_desk;
-	double acc_ener_desk;
-	double acc_ener_system;
-	double acc_comfort_error_desk;
-	double acc_comfort_var_desk;
-	double[BUFFER_SIZE] illuminance_min;
-	double[BUFFER_SIZE] duty_cycle_min;
