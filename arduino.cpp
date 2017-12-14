f06db4667268bab48ece4b726b241cc0da75637d
rmborges <rafael.m.borges@tecnico.ulisboa.pt>
2017-12-14 18:14:15 +0000

    arduino files

--- arduino.cpp
+++ arduino.cpp
@@ -2,8 +2,20 @@
 
 arduino::arduino()
 : occupancy(true)
-{
-
-}
+{}
 
 bool arduino::restart = false;
+
+void arduino::calc_inst_power_desk() {
+	inst_power_desk = duty_cycle/255*1;		// %PWM * nominal power (1W)
+}
+
+void arduino::calc_inst_power_system() {
+	
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
--- arduino.h
+++ arduino.h
@@ -7,8 +7,7 @@
 #include <boost/asio.hpp>
 #include <iostream>
 #include <unistd.h>
-
-#define BUFFER_SIZE 10000 // CORRIGIR VALOR!
+#include <vector>
 
 using namespace boost::asio;
 using ip::tcp;
@@ -19,31 +18,58 @@ class arduino {
 public:
 	arduino();
 
-private:	
-	//void start_serial(io_service& io_serial, serial_port *sp);
+	// getters
+	bool get_restart() {return restart;}
+	float get_inst_power_system() {return inst_power_system;}
+	float get_acc_comfort_error_system() {return acc_comfort_error_system;}
+	float get_acc_comfort_var_system() {return acc_comfort_var_system;}
+	float get_illuminance() {return illuminance;}
+	float get_duty_cycle() {return duty_cycle;}
+	bool get_occupancy() {return occupancy;}
+	float get_lower_bound() {return lower_bound;}
+	float get_ext_illuminance() {return ext_illuminance;}
+	float get_illuminance_ref() {return illuminance_ref;}
+	float get_inst_power_desk() {return inst_power_desk;}
+	float get_acc_ener_desk() {return acc_ener_desk;}
+	float get_acc_ener_system() {return acc_ener_system;}
+	float get_acc_comfort_error_desk() {return acc_comfort_error_desk;}
+	float get_acc_comfort_var_desk() {return acc_comfort_var_desk;}
+	std::vector<float> get_illuminance_min() {return illuminance_min;}
+	std::vector<float> get_duty_cycle_min() {return duty_cycle_min;}
+	std::vector<float> get_l_buffer() {return l_buffer;}
+	std::vector<float> get_d_buffer() {return d_buffer;}
 
+	//setters
+	void set_restart(bool res) {restart = res;}
+	void set_occupancy(bool occup) {occupancy = occup;}
+
+	//calculations
+	void calc_inst_power_desk();
+	void calc_inst_power_system();
+
+private:
 	// common to all arduinos
-	static bool restart;
-	static double inst_power_system;
-	static double acc_comfort_error_system;
-	static double acc_comfort_var_system;
+	static bool restart; // necessário? ou enviar so mensagem?
+	static float inst_power_system;
+	static float acc_comfort_error_system;
+	static float acc_comfort_var_system;
 
 	//for each arduino
-	double illuminance;
-	double duty_cycle;
+	float illuminance; //LDR
+	float duty_cycle; //PWM
 	bool occupancy;
-	double lower_bound;
-	double ext_illuminance;
-	double illuminance_ref;
-	double inst_power_desk;
-	double acc_ener_desk;
-	double acc_ener_system;
-	double acc_comfort_error_desk;
-	double acc_comfort_var_desk;
-	std::vector<double> illuminance_min;
-	std::vector<double> duty_cycle_min;
-
-
+	float lower_bound; // lux_ref - delta ?
+	float ext_illuminance; // LDR com luzes desligadas
+	float illuminance_ref; // lux_ref
+	float inst_power_desk;
+	float acc_ener_desk;
+	float acc_ener_system;
+	float acc_comfort_error_desk;
+	float acc_comfort_var_desk;
+	std::vector<float> illuminance_min;
+	std::vector<float> duty_cycle_min;
+	std::vector<float> l_buffer;
+	std::vector<float> d_buffer;
 
 };
 
