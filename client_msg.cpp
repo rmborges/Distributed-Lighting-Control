df80de0665aa255a130562ff1d6fa3331009c400
pgmacedo <paulogranjamacedo@gmail.com>
2017-12-17 17:41:17 +0000

    asd

--- client_msg.cpp
+++ client_msg.cpp
@@ -8,5 +8,5 @@ void client_msg::print_message(std::string message, char * msg_to_client){
 
 	std::cout << "\nno messafes: "<< message << std::endl;
 
-	msc_to_client = "oii, tudo bem?\n";
+	strcpy(msg_to_client, "oii, tudo bem?\n");
 }
